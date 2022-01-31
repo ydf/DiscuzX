782c6267c7b4a30be02b4f9fd0a4c5f176b7aa93
admin <admin@hongmi.local>
2022-01-31 17:40:07 +0800

    make subtitle < 280

--- upload/source/function/function_post.php
+++ upload/source/function/function_post.php
@@ -369,7 +369,7 @@ function checkmaxperhour($type) {
 
 function checkpost($subject, $message, $special = 0) {
 	global $_G;
-	if(dstrlen($subject) > 80) {
+	if(dstrlen($subject) > 280) {
 		return 'post_subject_toolong';
 	}
 	if(!$_G['group']['disablepostctrl'] && !$special) {
