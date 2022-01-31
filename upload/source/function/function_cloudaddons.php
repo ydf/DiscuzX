b4169e335892a0340cdc08def299a4b7dbbfa981
admin <admin@hongmi.local>
2022-01-31 18:48:11 +0800

    去除正版应用检测

--- upload/source/function/function_cloudaddons.php
+++ upload/source/function/function_cloudaddons.php
@@ -115,10 +115,7 @@ function cloudaddons_removelog($rid) {
 }
 
 function cloudaddons_validator($addonid) {
-	$array = cloudaddons_getmd5($addonid);
-	if(cloudaddons_open('&mod=app&ac=validator&ver=2&addonid='.$addonid.($array !== false ? '&rid='.$array['RevisionID'].'&sn='.$array['SN'].'&rd='.$array['RevisionDateline'] : '')) === '0') {
-		cpmsg('cloudaddons_genuine_message', '', 'error', array('addonid' => $addonid));
-	}
+		
 }
 
 function cloudaddons_upgradecheck($addonids) {
